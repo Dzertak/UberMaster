f9917992e73584ae89c036d681265d5b85f51598
Mary Ry <marytratata@gmail.com>
2017-11-20 23:39:01 +0200

    Rubanenko, add package master

---
+++ src/master/Master.java
@@ -0,0 +1,39 @@
+package master;
+
+import java.util.Date;
+
+public class Master extends MasterModel{
+
+
+    public String getProfession() {
+        return profession;
+    }
+
+    public String getSkills() {
+        return skills;
+    }
+
+    public String getExperience() {
+        return experience;
+    }
+
+    public int getPayment() {
+        return payment;
+    }
+
+    public boolean isSmoke() {
+        return smoke;
+    }
+
+    public String getTools() {
+        return tools;
+    }
+
+    public Date getStart_time() {
+        return start_time;
+    }
+
+    public Date getEnd_time() {
+        return end_time;
+    }
+}
---
+++ src/master/MasterAttr.java
@@ -0,0 +1,16 @@
+package master;
+
+import user.UserAttr;
+
+public interface MasterAttr extends UserAttr {
+    String M_OBJTYPE = "2";
+
+    String PROFESSION =  "6";
+    String SKILLS = "7";
+    String EXPERIENCE = "8";
+    String PAYMENT = "9";
+    String SMOKE = "10";
+    String TOOLS = "11";
+    String ST_TIME = "12";
+    String END_TIME = "13";
+}
