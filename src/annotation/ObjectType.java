657aa44d60bb8274a5a507d127a2920b2690bf0e
Oleksii Kravchenko <torlindikai@rambler.ru>
2017-11-21 13:00:32 +0200

    add Admin entity and change hierarchy

--- src/annotation/ObjectType.java
+++ src/annotation/ObjectType.java
@@ -4,6 +4,7 @@ import java.lang.annotation.Inherited;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 
+
 @Inherited
 @Retention(RetentionPolicy.RUNTIME)
 public @interface ObjectType
