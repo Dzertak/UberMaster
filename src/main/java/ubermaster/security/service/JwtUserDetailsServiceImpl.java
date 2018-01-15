ce4341425280c9e4d1811b0cb2dc8788b83deec4
Vitaly <meloman787@gmail.com>
2018-01-15 18:52:49 +0200

    Refactoring + user blocking check (throw exception if blocked)

--- src/main/java/ubermaster/security/service/JwtUserDetailsServiceImpl.java
+++ src/main/java/ubermaster/security/service/JwtUserDetailsServiceImpl.java
@@ -4,13 +4,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+import ubermaster.entity.model.BlockedUser;
 import ubermaster.entity.model.User;
 import ubermaster.entity.security.JwtUser;
 import ubermaster.persistence.facade.Facade;
 
-/**
- * Created by stephan on 20.03.16.
- */
 @Service
 public class JwtUserDetailsServiceImpl<T extends User> implements UserDetailsService {
 
@@ -21,10 +19,15 @@ public class JwtUserDetailsServiceImpl<T extends User> implements UserDetailsSer
     public JwtUser loadUserByUsername(String phoneNumber) throws UsernameNotFoundException {
         T user = facade.getUserByPhone(phoneNumber);
 
-        if (user == null) {
-            throw new UsernameNotFoundException("No user found with phone number: " + phoneNumber);
-        } else {
-            return new JwtUser<>(user);
+        if (user != null) {
+            if (BlockedUser.class.isAssignableFrom(user.getClass())) {
+                if (((BlockedUser) user).getBlocked()) {
+                    throw new UsernameNotFoundException("User is blocked ");
+                }
+                return new JwtUser(user);
+            }
+            return new JwtUser(user);
         }
+        throw new UsernameNotFoundException("No user found with phone number: " + phoneNumber);
     }
 }
