0f5bc39e98d1ff0350d40819319ef963eac28fd0
JohnCalen <vivaleute@gmail.com>
2018-02-05 12:42:02 +0200

    converterTEST (convertStringToObject)

--- src/main/test/ConverterTEST.java
+++ src/main/test/ConverterTEST.java
@@ -2,6 +2,9 @@ import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import ubermaster.persistence.converter.impl.ConverterImpl;
+
+import java.text.ParseException;
+
 import static org.junit.Assert.*;
 public class ConverterTEST
 {
@@ -44,5 +47,33 @@ public class ConverterTEST
 	{
 		Object coverted = ConverterImpl.convertObjectToString(true);
 		assertEquals("Must be", "true", coverted);
+	}
+
+	@Test
+	public void convertStringToStringObject() throws ParseException
+	{
+		Object converted = ConverterImpl.convertStringToObject("StrinG", String.class);
+		assertEquals("Must be", "StrinG", converted);
+	}
+
+	@Test
+	public void convertStringToLong() throws ParseException
+	{
+		Object converted = ConverterImpl.convertStringToObject("12314545436577345", long.class);
+		assertEquals("Must be", 12314545436577345L, converted);
+	}
+
+	@Test
+	public void convertStringToBoolean() throws ParseException
+	{
+		Object converted = ConverterImpl.convertStringToObject("true", boolean.class);
+		assertEquals("Must be", true, converted);
+	}
+
+	@Test
+	public void convertNullToObject() throws ParseException
+	{
+		Object converted = ConverterImpl.convertStringToObject(null, boolean.class);
+		assertEquals("Must be", null, converted);
 	}
 }
